39bde27f65ba5388c9f1bc647d5f54a2ace0efad
tokkaiiii <luv_3_u@naver.com>
2025-11-28 09:56:22 +0900

    feat: test 유틸 추가

---
+++ src/test/java/study/hellocache/api/ItemTestUtils.java
@@ -0,0 +1,60 @@
+package study.hellocache.api;
+
+import org.springframework.web.client.RestClient;
+import study.hellocache.common.cache.CacheStrategy;
+import study.hellocache.model.ItemCreateRequest;
+import study.hellocache.model.ItemUpdateRequest;
+import study.hellocache.service.response.ItemPageResponse;
+import study.hellocache.service.response.ItemResponse;
+
+public class ItemTestUtils {
+    static RestClient restClient = RestClient.create("http://localhost:8080");
+
+    static ItemResponse read(CacheStrategy cacheStrategy, Long itemId) {
+        return restClient.get()
+                .uri("/cache-strategy/%s/items/%s".formatted(cacheStrategy.name(), itemId))
+                .retrieve()
+                .body(ItemResponse.class);
+    }
+
+    static ItemPageResponse readAll(CacheStrategy cacheStrategy, long page, long pageSize) {
+        return restClient.get()
+                .uri("/cache-strategy/%s/items?page=%d&pageSize=%d".formatted(cacheStrategy.name(), page, pageSize))
+                .retrieve()
+                .body(ItemPageResponse.class);
+    }
+
+    static ItemPageResponse readAllInfiniteScroll(CacheStrategy cacheStrategy, Long lastItemId, Long pageSize) {
+        return restClient.get()
+                .uri(
+                        lastItemId == null
+                                ? "/cache-strategy/%s/items/infinite-scroll?pageSize=%s".formatted(cacheStrategy.name(), pageSize)
+                                : "/cache-strategy/%s/items/infinite-scroll?lastItemId=%s&pageSize=%s".formatted(cacheStrategy.name(), lastItemId, pageSize)
+                )
+                .retrieve()
+                .body(ItemPageResponse.class);
+    }
+
+    static ItemResponse create(CacheStrategy cacheStrategy, Long itemId, ItemCreateRequest request) {
+        return restClient.post()
+                .uri("/cache-strategy/%s/items/%s".formatted(cacheStrategy.name(), itemId))
+                .body(request)
+                .retrieve()
+                .body(ItemResponse.class);
+    }
+
+    static ItemResponse update(CacheStrategy cacheStrategy, Long itemId, ItemUpdateRequest request) {
+        return restClient.put()
+                .uri("/cache-strategy/%s/items/%s".formatted(cacheStrategy.name(), itemId))
+                .body(request)
+                .retrieve()
+                .body(ItemResponse.class);
+    }
+
+    static void delete(CacheStrategy cacheStrategy, Long itemId) {
+        restClient.delete()
+                .uri("/cache-strategy/%s/items/%s".formatted(cacheStrategy.name(), itemId))
+                .retrieve()
+                .toBodilessEntity();
+    }
+}
